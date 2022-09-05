171a5be2b09a83bcfc936baa7aa2aa2a98b43ff1
mehmetyusufdal <mehmetyusufdal@gmail.com>
2022-09-05 09:34:21 +0300

    lru_cache.hpp doxygen documentation

--- lib/include/lru_cache.hpp
+++ lib/include/lru_cache.hpp
@@ -1,20 +1,13 @@
 /**
  * @file lru_cache.hpp
  * @author Mehmet Yusuf Dal (mehmetyusufdal@gmail.com)
- * @brief 
- * @version 0.1
+ * @brief LRU Cache Implementation
  * @date 2022-08-25
- * 
- * @copyright Copyright (c) 2022
- * 
  */
 #pragma once
 
 #include <unordered_map>
 #include <list>
-
-#include <iostream>//temp
-#include <string>//temp
 
 template <class key_T, class val_T>
 class LRUCache {
@@ -25,25 +18,39 @@ private:
 	std::list<key_T> cache{};
 	std::unordered_map<key_T, val_T> data{};
 
-    typedef std::pair<key_T, val_T> data_pair;
+	typedef std::pair<key_T, val_T> data_pair;
 
 public:
+	/**
+	 * @brief Construct a new LRUCache object
+	 * 
+	 * @param capacity Capacity of hashmap
+	 */
 	LRUCache(std::size_t capacity) : capacity(capacity) {}
 
-	void print() const {//temp
-		for (auto t : cache) {
-			std::cout << std::to_string(t) << " ";
-		}
-		printf("\n");
-	}//temp
-
+	/**
+	 * @brief Set the capacity of hashmap
+	 * 
+	 * @param capacity Capacity of hashmap
+	 */
 	void set_capacity(std::size_t capacity) noexcept { this->capacity = capacity; }
 
+	/**
+	 * @brief Clear LRUCache
+	 */
 	void clear() noexcept {
 		cache.clear();
 		data.clear();
 	};
 
+	/**
+	 * @brief Append new data into hashmap with key & value parameters
+	 * 
+	 * @param key 
+	 * @param val 
+	 * @return true if data successfully appended
+	 * @return false if data cannot be appended
+	 */
 	bool append(key_T const& key, val_T const& val) {
 		if (data.find(key) == data.end()) {
 			if (data.size() + 1 > capacity) {
@@ -52,47 +59,76 @@ public:
 			}
 			data.emplace(key, val);
 			cache.push_front(key);
-			print();//temp
 			return true;
 		}
-		//else { } // Note: Maybe throw exception?
+		// else { } // Note: Maybe throw exception?
 		return false;
 	}
 
+	/**
+	 * @brief Append new data into hashmap with std::pair<key, value>
+	 * 
+	 * @param val 
+	 * @return true if data successfully appended
+	 * @return false if data cannot be appended
+	 */
 	bool append(data_pair const& val) {
 		return append(val.first, val.second);
 	}
 
+	/**
+	 * @brief Find given key in hashmap
+	 * 
+	 * @param key 
+	 * @return reference to given key in hashmap
+	 */
 	auto find(key_T const& key) -> decltype(data.begin()) {
 		auto return_val = data.find(key);
 		if (return_val != data.end()) {
 			auto t = std::find(cache.begin(), cache.end(), key);
 			cache.splice(cache.begin(), cache, t);
-			print();//temp
 		}
 		return return_val;
 	}
 
-    bool erase(key_T const& key) {
-        auto iter = find(key);
-        if(iter != data.end()) {
-            data.erase(iter);
-            cache.pop_front();
-            print();//temp
-            return true;
-        }
-        print();//temp
-        return false;
-    }
+	/**
+	 * @brief Erase given key and its value from hashmap
+	 * 
+	 * @param key 
+	 * @return true if successfully erased
+	 * @return false if can not be erased
+	 */
+	bool erase(key_T const& key) {
+		auto iter = find(key);
+		if (iter != data.end()) {
+			data.erase(iter);
+			cache.pop_front();
+			return true;
+		}
+		return false;
+	}
 
+	/**
+	 * @brief Get the value of given key
+	 * 
+	 * @param key 
+	 * @return value of given key
+	 * @exception std::runtime_error key not found in the hashmap
+	 */
 	val_T get_value(key_T const& key) {
 		auto iter = find(key);
 		[[unlikely]] if (iter == data.end()) throw(std::runtime_error(except_msg));
 		return iter->second;
 	}
 
-	val_T operator [] (key_T const& key) {
+	/**
+	 * @brief Get the value of given key
+	 * 
+	 * @param key 
+	 * @return value of given key
+	 * @exception std::runtime_error key not found in the hashmap
+	 */
+	val_T operator[](key_T const& key) {
 		return get_value(key);
 	}
-	
 };

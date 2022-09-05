c4576b1414b3c22956edf82d18635c66797ddabe
mehmetyusufdal <mehmetyusufdal@gmail.com>
2022-09-05 15:26:13 +0300

    binary_tree.hpp initial commit

---
+++ lib/include/binary_tree.hpp
@@ -0,0 +1,66 @@
+/**
+ * @file binary_tree.hpp
+ * @author Mehmet Yusuf Dal (mehmetyusufdal@gmail.com)
+ * @brief Binary Tree Implementation
+ * @date 2022-09-05
+ */
+#pragma once
+
+#include <concepts>
+#include <iostream> // temp
+
+template <class T>
+concept aritmetic = std::integral<T> || std::floating_point<T>;
+
+template <aritmetic T = int>
+class BinaryTree{
+private:
+    struct Node;
+    typedef std::unique_ptr<Node> node_ptr;
+
+    struct Node{
+        T value{ NULL };
+        node_ptr left{ nullptr };
+        node_ptr right{ nullptr };
+    };
+
+    node_ptr root{ nullptr };
+
+    void append(node_ptr& node, T const& val) {
+        if(!node) {
+            node = std::make_unique<Node>();
+            node->value = val;
+        }
+        else if (val >= node->value) append(node->right, val);
+        else append(node->left, val);
+    }
+
+    void print(node_ptr& node) { // temp
+        if(node->left) print(node->left);
+        
+        std::cout << std::to_string(node->value) << " ";
+        
+        if(node->right) print(node->right);
+    } // temp
+
+public:
+    BinaryTree() {}
+
+    BinaryTree(T const& root_val) {
+        root = std::make_unique<Node>();
+        root->value = root_val;
+    }
+
+    void print() { // temp
+        print(root);
+    } // temp
+
+    void append(T const& val) {
+        if(!root) {
+            root = std::make_unique<Node>();
+            root->value = val;
+        }
+        else if(val >= root->value) append(root->right, val);
+        else append(root->left, val);
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <string>
-#include <lru_cache.hpp>
+#include <binary_tree.hpp>
 
 int main(int, char **) {
+
+	BinaryTree<float> k(5.2);
+
+	k.append(3);
+	k.append(4.65);
+	k.append(1.255);
+	k.append(2);
+	k.append(6);
+
+	k.print();
 
 	return 0;
 }
